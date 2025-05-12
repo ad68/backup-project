4946afadb09ab6370ce6113001552248b9b93830
unknown <alidargahi1368@gmail.com>
2025-05-12 23:57:11 +0330

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <section className='w-[100px] h-[100px] bg-[#ffff00]'>hi ali</section>
+      <section className='w-[100px] h-[100px] bg-[red]'>hi ahmad</section>
     </>
   )
 }
