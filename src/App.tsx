28fe663c3d520bbe7c844b418cd96b5abc059ef5
unknown <alidargahi1368@gmail.com>
2025-05-13 00:03:42 +0330

    aadlkasd

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <section className='w-[100px] h-[100px] bg-[purple]'>hi llll</section>
+      <section className='w-[100px] h-[100px] bg-[brown]'>hi 456</section>
     </>
   )
 }
