ef05882858b818b601e1c5616bf42a3856ca57f2
unknown <alidargahi1368@gmail.com>
2025-05-13 00:33:36 +0330

    lskdlfksdf

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <section className='w-[100px] h-[100px] bg-[brown]'>hi 456</section>
+      <section className='w-[100px] h-[100px] bg-[black]'>hi 554345345</section>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
-
 import './index.css'
 import App from './App.tsx'
 
-registerSW()
 const updateSW = registerSW({
   onNeedRefresh() {
-
     const confirmed = window.confirm('نسخه جدید در دسترس است. آیا می‌خواهید بروزرسانی کنید؟')
     if (confirmed) {
       updateSW(true)
     }
   },
   onOfflineReady() {
-    console.log('اپ آماده استفاده آفلاینه 🚀')
+    console.log('اپ آماده استفاده آفلاینه')
   },
 })
 createRoot(document.getElementById('root')!).render(
