ea97c7f7e1892d44db955f5f2daa6f376970af33
unknown <alidargahi1368@gmail.com>
2025-05-13 20:19:09 +0330

    add

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header'
 export default function Index() {
     return <section className="m-auto w-[440px] h-screen max-w-full relative">
         <Header />
-        <section className='w-full min-h-[400px] h-[calc(100%-220px)] bg-gradient-to-b from-slate-50 to-green-50 mt-[-80px] box-border relative rounded-t-[40px] z-50'>
+        <section className='w-full min-h-[400px]  h-[calc(100%-220px)] bg-gradient-to-b from-slate-50 to-green-50 mt-[-80px] box-border relative rounded-t-[40px] z-50'>
             <section className='w-full absolute top-[-60px] flex justify-center'>
                 <section className='w-[290px] px-[20px] flex justify-between items-center gap-4 h-[130px] m-auto bg-white rounded-[40px] shadow-2xl'>
                     <section className=' flex justify-between items-start gap-3'>
@@ -24,6 +24,20 @@ export default function Index() {
                     </section>
                 </section>
             </section>
+            <section className='h-full  gap-2 pt-[100px] pb-[80px] overflow-scroll rounded-t-[40px]'>
+                <section className='w-[90%] h-[90px] m-auto bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+                <section className='w-[90%] h-[90px] m-auto mt-3 bg-slate-200  rounded-lg border'></section>
+
+            </section>
+
         </section>
-    </section>;
+    </section>
 }
