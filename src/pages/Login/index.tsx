16d2a6c87be717e6e617c70509fd9d84d955c011
ad68 <47919905+ad68@users.noreply.github.com>
2025-05-21 19:20:57 +0330

    .

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,8 +2,8 @@ import LoginForm from './components/LoginForm'
 export default function Index() {
     return <section className="m-auto relative bg-primary w-[440px] h-screen max-w-full">
         <section className="absolute  w-full top-0  h-[400px] left-0 overflow-hidden">
-            <img src="/public/images/login/sss.png" className="w-[80px] top-[5px] rotate-[10deg] absolute left-0" alt="" />
-            <img src="/public/images/login/ir-white.svg" className="w-[150px] top-[20px] absolute right-2" alt="" />
+            <img src="/images/login/sss.png" className="w-[80px] top-[5px] rotate-[10deg] absolute left-0" alt="" />
+            <img src="/images/login/ir-white.svg" className="w-[150px] top-[20px] absolute right-2" alt="" />
             <section className='w-full text-center text-white text-[32px] font-bold mt-[180px]'>بکاپ</section>
             <span className='block mt-[5px] text-white w-full text-center'>بیمه کشاورزی ایرانیان پوشش</span>
         </section>
