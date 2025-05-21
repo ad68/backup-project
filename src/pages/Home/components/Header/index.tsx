9a19271dd881ce0855e92661b21ae1bb86377c78
ad68 <47919905+ad68@users.noreply.github.com>
2025-05-21 21:15:33 +0330

    .

--- src/pages/Home/components/Header/index.tsx
+++ src/pages/Home/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Notification from './components/Notification'
 import Wallet from './components/Wallet'
 import ActionsCount from './components/ActionsCount'
 export default function Index() {
-    return <section className="h-[310px] bg-gradient-to-b from-primary to-[#247f4d] overflow-hidden relative z-0">
+    return <section className="h-[310px] rounded-b-[82px] bg-gradient-to-b from-primary to-[#247f4d] overflow-hidden relative z-0">
         <section className='flex items-center justify-between py-2 px-3'>
             <Profile />
             <Notification />
