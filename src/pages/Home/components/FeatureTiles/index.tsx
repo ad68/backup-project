8d330e71aead8a214faad11e7168893fad0981c9
ad68 <47919905+ad68@users.noreply.github.com>
2025-05-24 09:59:16 +0330

    .

--- src/pages/Home/components/FeatureTiles/index.tsx
+++ src/pages/Home/components/FeatureTiles/index.tsx
@@ -4,6 +4,7 @@ import { FireIcon } from "@/assets/icons/FireIcon";
 import { WalletIcon } from '@/assets/icons/WalletIcon';
 import { AttachIcon } from '@/assets/icons/AttachIcon';
 import { ReportIcon } from '@/assets/icons/ReportIcon';
+import { TeachIcon } from '@/assets/icons/TeachIcon';
 export default function Index() {
     return <section>
         <section className="pt-[110px] px-5">
@@ -12,7 +13,8 @@ export default function Index() {
                 <Tile iconBgColor='bg-[#effff2]' borderColorClass={'border-primary'} title="پیوست فنی" image={<AttachIcon className="text-primary stroke-primary w-[25px]" />} />
                 <Tile iconBgColor='bg-[#fffaf4]' borderColorClass={'border-orange-300'} title="خسارت" image={<FireIcon className="text-orange-300 w-[25px]" />} />
                 <Tile iconBgColor='bg-[#fff5f5]' borderColorClass={'border-red-400'} title="گزارشات" image={<ReportIcon className="text-red-400 w-[21px]" />} />
-
+                <Tile iconBgColor='bg-[#ffffeb]' borderColorClass={'border-yellow-400'} title="آموزش" image={<TeachIcon className="text-yellow-400 w-[21px]" />} />
+                <Tile iconBgColor='bg-[#ffffeb]' borderColorClass={'border-[#15cefa]'} title="قوانین و قررات" image={<TeachIcon className="text-[#15cefa] w-[21px]" />} />
             </section>
             <section className='flex-center mt-5'>
                 <Tile width="157px" iconBgColor='bg-[#fbf2fb]' borderColorClass={'border-purple-400'} title="کیف پول" image={<WalletIcon className="text-purple-400 w-[25px]" />} />
