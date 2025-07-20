95359e3284473330e7bd97b6c1df94dd60dc6eb5
unknown <alidargahi1368@gmail.com>
2025-07-20 21:55:37 +0330

    .

--- src/modules/Home/components/Header/components/ActionsCount/index.tsx
+++ src/modules/Home/components/Header/components/ActionsCount/index.tsx
@@ -2,7 +2,7 @@ import Item from './components/Item'
 
 export default function Index() {
     return <section className="flex gap-2 justify-between px-4 mt-8">
-        <Item title="پیوست های فنی" count={7} />
+        <Item title="پیوست های فنی" count={0} />
         <Item title="پایش فنولوژی" count={0} />
         <Item title="خسارت" count={0} />
     </section>
