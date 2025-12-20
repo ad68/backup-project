815b8ef96ea48872580d302878302673a16353a1
a.dargahi <alidargahi1368@gmail.com>
2025-12-20 21:34:39 +0330

    .

--- src/modules/Login/index.tsx
+++ src/modules/Login/index.tsx
@@ -6,11 +6,11 @@ export default function Index() {
             <section className="absolute w-full top-0 h-[400px] left-0 overflow-hidden">
                 <img src="/images/login/sss.png" className="w-[80px] top-[5px] rotate-[10deg] absolute left-0" alt="" />
                 <section className=' flex justify-center w-full pl-2 mt-5'>
-                    <img src="/images/login/sandoogh-logo.svg" className="w-[110px] mr-3" alt="" />
+                    <img src="/images/login/sandoogh-logo.svg" className="w-[110px] mt-[30px]" alt="" />
                 </section>
                 <img src="/images/login/sss.png" className="w-[80px] top-[5px] absolute rotate-[-10deg] right-0 scale-x-[-1]" alt="" />
-                {/*                <section className='w-full text-center text-white text-[28px] font-bold mt-[20px]'>بکاپ</section> */}
-                <span className='block mt-[5px] text-white w-full text-center text-[18px] font-bold'>سامانه ارزیابی صندوق بیمه کشاورزی</span>
+                {/*<section className='w-full text-center text-white text-[28px] font-bold mt-[20px]'>بکاپ</section> */}
+                <span className='block mt-[20px] text-white w-full text-center text-[20px] font-bold'>سامانه ارزیابی صندوق بیمه کشاورزی</span>
             </section>
             <LoginForm />
         </section>
