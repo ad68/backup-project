f83bb359a5a416fe8158d96ac602d5daa0b2599f
a.dargahi <alidargahi1368@gmail.com>
2025-12-20 21:31:24 +0330

    .

--- src/modules/Home/Home.biz.tsx
+++ src/modules/Home/Home.biz.tsx
@@ -2,16 +2,9 @@ import { toastError } from '@/components/kit/toast'
 import BASE_URL from '@/config/api'
 import { useAxiosWithToken } from '@/hooks'
 import { useAuthStore } from '@/store/authStore'
-
-
-//
-// ──────────────────────────────────────────────────────────────
-//   :::::: B U S I N E S S : :  :   :    :     :        :          :
-// ──────────────────────────────────────────────────────────────
-//
 const useHome = () => {
+    const setUserInfo = useAuthStore((state) => state.setUserInfo)
     const getUserInfo = () => {
-        const setUserInfo = useAuthStore((state) => state.setUserInfo)
         useAxiosWithToken.post(BASE_URL + "/sabka/sso/auth").then((res) => {
             setUserInfo(res.data.sabkaActor)
         }).catch(err => {
--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -2,11 +2,13 @@ import Header from './components/Header'
 import FeatureTiles from './components/FeatureTiles'
 import WeatherWidget from './components/WeatherWidget'
 import useHome from './Home.biz'
-
+import { useEffect } from 'react'
 
 export default function Index() {
     const { getUserInfo } = useHome()
-    getUserInfo()
+    useEffect(() => {
+        getUserInfo()
+    }, [])
     return <section className="m-auto w-full max-w-7xl relative">
         <Header />
         <section className='w-full bg-gradient-to-b mt-[-40px] box-border relative z-50'>
